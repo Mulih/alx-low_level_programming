737c3049c3adfccac77cfb62b217cce3c418d765
Mulih <almulih@yahoo.com>
2022-12-09 14:18:09 -0800

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase
+ *
+ *  Return: (0)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = a; letter < z; a++)
+		putchar(letter);
+
+	putchar("\n");
+	return (0);
+}
