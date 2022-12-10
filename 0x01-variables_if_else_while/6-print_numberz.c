5693f99ec12e65f713f5660784560056cb04acc7
Mulih <almulih@yahoo.com>
2022-12-10 03:59:20 -0800

    t6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,14 +9,16 @@ int main(void)
 {
 	int i;
 
-	i = 0;
-
-	while (i != 10)
+	for (i = 0; i <= 10; i++)
+	{
+		i = 0;
+		while (i != 10)
 		{
 			putchar(i);
 			i++;
 		}
-
+	}
+	
 	putchar('\n');
 	return (0);
 }
