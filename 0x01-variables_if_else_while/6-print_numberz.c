41c3809a80452cf5762c38a78cc5efe34f7551a6
Mulih <almulih@yahoo.com>
2022-12-09 18:22:28 -0800

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - a program that prints all single digit numbers of base 10.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num);
+	}
+
+	putchar('\n');
+	return (0);
+}
