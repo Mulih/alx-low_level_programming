294bf79687208100998b2881125d1d1ce79a9822
Mulih <almulih@yahoo.com>
2022-12-11 02:55:26 -0800

    10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print all possible combintations of two digits.
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	int n1, n2;
+
+	for (n1 = 0; n1 <= 9; n2++)
+	{
+		for (n2 = n1 + 1; n2 <= 8; n2++)
+		{
+			putchar(n1 + '0');
+			putchar(n2 + '0');
+			if (n1 != 9)
+			{
+				putchar(' ');
+				putchar(',');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
+
