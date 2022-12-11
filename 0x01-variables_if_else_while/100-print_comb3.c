b2869d808a82637126428928fa29dc9d3525ec28
Mulih <almulih@yahoo.com>
2022-12-11 03:44:47 -0800

    10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,14 +12,14 @@ int main(void)
 
 	for (n1 = 0; n1 <= 9; n1++)
 	{
-		for (n2 = n1 + 1; n2 <= 9; n2++)
+		for (n2 = n1 + 1; n2 <= 9; n1++)
 		{
 			putchar(n1 + '0');
 			putchar(n2 + '0');
 			if (n1 != 8)
 			{
-				putchar(' ');
 				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
