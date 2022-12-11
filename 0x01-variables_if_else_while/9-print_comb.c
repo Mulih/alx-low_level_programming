71510e6005dab63df87727b1afdb09ae44df8dae
Mulih <almulih@yahoo.com>
2022-12-11 01:02:16 -0800

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Print all possible combination of single digit numbers
+ *
+ * Return: (0)
+ *
+ */
+int main(void)
+{
+	int n;
+
+	n = (0);
+
+	for (n = 0; n < 10; n++)
+	{
+		if (n < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
