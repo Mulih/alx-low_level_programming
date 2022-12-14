05330a3c2827512a7e1b151c3c01bc9044ccb982
Mulih <almulih@yahoo.com>
2022-12-14 05:50:36 -0800

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabet in lowercase.
+ *
+ * Return: 0
+ *
+ */
+void print_alphabet(void)
+{
+	int start = 97, stop = 122;
+
+	while (start <= stop)
+	{
+		+putchar(start);
+		start++;
+	}
+	_putchar('\n');
+	return (0);
+}
+
