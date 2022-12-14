e1f76a52c779bc328a5c39ced71da81c765f70cd
Mulih <almulih@yahoo.com>
2022-12-14 10:03:34 -0800

    task 6, 7 & 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * jack_bauer - print every minute of Jack Bauer's day, \
+ starting from 0:00 to 23:59.
+ * 
+ * return: nothing
+ */
+void jack_bauer(void)
+{
+	int a = 0, b = 23, a2 = 0, b2 = 59;
+
+	while (a <= b)
+	{
+		while (a2 <= b2)
+		{
+			_putchar(a < 9 ? 0 + '0' : a / 10 + '0');
+			_putchar(a < 9 ? a + '0' : a % 10 + '0');
+			_putchar(':');
+			_putchar(a2 < 9 ? 0 + '0' : a2 / 10 + '0');
+			_putchar(a2 < 9 ? a2 + '0' : a2 % 10 + '0');
+			_putchar('\n');
+
+			a2++;
+		}
+		a2 = 0;
+		a++;
+	}
+}
