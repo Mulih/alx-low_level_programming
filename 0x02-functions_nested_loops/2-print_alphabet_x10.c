e909b60bab9e33b7ba5d32d526fbdd9b6fc52e2d
Mulih <almulih@yahoo.com>
2022-12-14 06:33:12 -0800

    task 2 corrections

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print the alphabet in lowercase ten times.
+ *
+ * Return: nothing
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int start = 97, stop = 122, count = 1;
+
+	while (count <= 10)
+	{
+		while (start <= stop)
+		{
+			_putchar(start);
+			start++;
+		}
+		_putchar('\n');
+
+		start = 97;
+		count++;
+	}
+}
