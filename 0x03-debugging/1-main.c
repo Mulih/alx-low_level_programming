09a26ce45c7ceff89a32c6254ca986080950f179
Mulih <almulih@yahoo.com>
2022-12-16 00:47:31 -0800

    correcting task 1 and updating README.

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,14 +10,14 @@ int main(void)
 	int i;
 
 	printf("infinite loop incoming :(\n");
-
 	i = 0;
 
-	/** while (1 < 10)
+	/**
+	 * while (1 < 10)
 	 * {
 	 *       putchar(i);
-	 * } */ 
-
+	 * }
+	 */
 	printf("infinite loop avoided! \\o/\n");
 
 	return (0);
