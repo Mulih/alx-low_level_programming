be2a7850922a0638b046713c22b352a460a83ecf
Mulih <almulih@yahoo.com>
2022-12-19 16:32:59 -0800

    corrections task 0 description

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,5 @@
 /**
- * reset_to_98 - takes the pointers to an int as parametersand updates the \ value it points to to 98.
+ * reset_to_98 - takes the pointers to an int as parametersand updates the \ value.
  * @n: number to be updated.
  *
  * Return: nothing on success.
