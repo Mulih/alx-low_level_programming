6c0a438e28379be048cb7f4db818f85b05b0506a
Mulih <almulih@yahoo.com>
2023-02-02 09:36:48 -0800

    Quick Fix

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -10,7 +10,7 @@ size_t free_listint_safe(listint_t **h);
  * return: if the list is not looped - 0,
  * otherwise - the number of unique nodes in the list
  */
-size-t looped_listint_count(listint_t *head)
+size_t looped_listint_count(listint_t *head)
 {
 	listint_t *tortoise, *hare;
 	size_t nodes = 1;
@@ -58,6 +58,16 @@ size_t free_listint_safe(listint_t **h)
 	if (nodes == 0)
 	{
 		for (; h != NULL && *h != NULL; nodes++)
+		{
+			tmp = (*h)->next;
+			free(*h);
+			*h = tmp;
+		}
+	}
+
+	else
+	{
+		for (index = 0; index < nodes; index++)
 		{
 			tmp = (*h)->next;
 			free(*h);
