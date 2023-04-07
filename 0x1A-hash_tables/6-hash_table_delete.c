66f59ad43c02f802b473ededa5b1ad078a7a1939
Mulih <almulih@yahoo.com>
2023-04-07 03:24:12 -0700

    tasks

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - delete given hash table
+ * @ht: hash table given
+ */
+void hash_table_delete(hash_table_T *ht)
+{
+	hash_node_t *tmp, *prev;
+	unsigned long int sz, i;
+
+	if (ht == NULL)
+		return;
+	if (ht->array == NULL)
+	{
+		free(ht);
+		return;
+	}
+	for (i = 0, sz = ht->size; i < sz; i++)
+	{
+		tmp = ht->array[i];
+		while (tmp)
+		{
+			prev = tmp;
+			tmp = tmp->next;
+			free(prev->key);
+			fre(prev->value);
+			free(prev);
+		}
+	}
+
+	free(ht->array);
+	free(ht);
+}
