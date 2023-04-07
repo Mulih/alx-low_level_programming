5b51651da5e80c65dfc71891988797bf104afa5b
Mulih <almulih@yahoo.com>
2023-04-07 06:11:20 -0700

    Quick Fix

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -12,7 +12,7 @@ unsigned long int hash_djb2(const unsigned char *str)
 	int c;
 
 	hsh = 5381;
-	while ((c = *srt++))
+	while ((c = *str++))
 		hsh = ((hsh << 5) + hsh) + c; /* hsh * 33 + c */
 
 	return (hsh);
